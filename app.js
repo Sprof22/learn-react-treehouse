8813840c589728afc479f744aec6a398d695d6e2
Sprof22 <richmondelaigwu@gmail.com>
2023-01-12 19:55:16 +0100

    small commits for the react way

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const title = React.createElement(
-    'h1',
-    {id: 'hello', title: 'this is cool'},
-    'My first React Element'
+const title = 'My first React Element';
+const desc = 'I just learnt how to render my first React Element';
+const myTitleId = 'hello-world'
+const header = (
+<header>
+    <h1 id = {myTitleId}>{title}</h1>
+    <p>{desc}</p>;
+</header>
 )
 
-console.log(title)
+ReactDOM.render(
+    header,
+    document.getElementById('root')
+);
