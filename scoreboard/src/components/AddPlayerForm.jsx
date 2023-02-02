3d6f18e9ba4524a94c9f5d4a9f1d6d74ded62f42
Sprof22 <richmondelaigwu@gmail.com>
2023-02-02 23:12:19 +0100

    oiu

--- scoreboard/src/components/AddPlayerForm.jsx
+++ scoreboard/src/components/AddPlayerForm.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 
-const AddPlayerForm = () => {
+const AddPlayerForm = (props) => {
   const [value, setValue] = useState();
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    props.AddPlayer(value);
+  }
   return (
-    <form>
+    <form onSubmit={(event)=> handleSubmit(event)}>
     {console.log(value)}
       <input type="text" placeholder="Enter a Player Name" value={value} onChange={(event) => setValue(event.target.value)} />
       <input type="submit" value="Add a Player" />
--- scoreboard/src/components/App.js
+++ scoreboard/src/components/App.js
@@ -26,6 +26,7 @@ const App = () => {
       id: 4,
     },
   ]);
+  const [nextPlayerId, setNextPlayerId] = useState(5)
 
   const handleRemovePlayer = (id) => {
     setPlayers((prevPlayers) => prevPlayers.filter((p) => p.id !== id));
@@ -45,6 +46,16 @@ const App = () => {
       })
     );
   };
+  const handleAddPlayer = (name)=> {
+    setPlayers([
+      {
+        name,
+        score: 0,
+        id: nextPlayerId
+    }
+  ])
+  setNextPlayerId(prevId => prevId +1)
+  }
   return (
     <div className="scoreboard">
       <Header title="Scoreboard" players={players} />
@@ -60,7 +71,7 @@ const App = () => {
           changeScore={handleScoreChange}
         />
       ))}
-      <AddPlayerForm />
+      <AddPlayerForm addPlayer={handleAddPlayer} />
     </div>
   );
 };
