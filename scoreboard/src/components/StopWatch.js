66bf13b110ce9ec7926342e69153d875ed9dfe1e
Sprof22 <richmondelaigwu@gmail.com>
2023-02-03 10:16:45 +0100

    stop watch component

---
+++ scoreboard/src/components/StopWatch.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const StopWatch = () => {
+  return (
+    <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <span className="stopwatch-time">0</span>
+        <button></button>
+    </div>
+  )
+}
+
+export default StopWatch
