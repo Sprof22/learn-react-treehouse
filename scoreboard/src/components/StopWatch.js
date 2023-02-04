89fe435a1ddc63591a39e7e6788a1f6a103dd6c0
Sprof22 <richmondelaigwu@gmail.com>
2023-02-04 23:09:20 +0100

    PROLLY NOTHING

--- scoreboard/src/components/StopWatch.js
+++ scoreboard/src/components/StopWatch.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const StopWatch = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elaspedTime, setElaspedTime] = useState(0);
+
+  
+
+  useEffect(()=> {
+    let id;
+    if(isRunning){
+      id = setInterval(()=> setElaspedTime(prevTime => prevTime +1), 1000)
+    }
+    return () => clearInterval(id);
+  },[isRunning])
   return (
     <div className="stopwatch">
       <h2>Stopwatch</h2>
-      <span className="stopwatch-time">0</span>
-      <button>Start</button>
-      <button>Reset</button>
+      <span className="stopwatch-time">{elaspedTime}</span>
+      <button onClick={() => setIsRunning((prevState) => !prevState)}>
+        {isRunning ? "Stop" : "Start"}
+      </button>
+      <button onClick={()=>setElaspedTime(0)}>Reset</button>
     </div>
   );
 };
