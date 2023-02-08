607c8ca32fc7770ca5238dee0c7e9e013aa88e6e
Sprof22 <richmondelaigwu@gmail.com>
2023-02-08 19:34:28 +0100

    fetch react

--- gif-search/src/App.js
+++ gif-search/src/App.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
-import SearchForm from './Components/SearchForm';
+import SearchForm from "./Components/SearchForm";
 import GifList from "./Components/GifList";
 
 function App() {
-  const [gifs, setGifs] = useState([])
+  const [gifs, setGifs] = useState([]);
   useEffect(() => {
-    fetch("https://api.giphy.com/v1/gifs/trending")
-  }, [])
-  
+    fetch(
+      "https://api.giphy.com/v1/gifs/trending?api_key=zH1fd1S5RPAAGv4Z4vA4qHL45kEicv3C&limit=25&rating=g"
+    )
+      .then(response => response.json())
+      .then(responseData => setGifs(responseData)).catch(error => console.log("Error fetching and Parsing data", error))
+  }, []);
+
   return (
     <div>
       <div className="main-header">
