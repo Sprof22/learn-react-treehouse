f817c1c2cd9101bd23bb07c15dd7ec5cbf02fd8a
Sprof22 <richmondelaigwu@gmail.com>
2023-02-08 19:39:26 +0100

    lets introduce axios

--- gif-search/src/App.js
+++ gif-search/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import SearchForm from "./Components/SearchForm";
 import GifList from "./Components/GifList";
 
@@ -8,8 +9,9 @@ function App() {
     fetch(
       "https://api.giphy.com/v1/gifs/trending?api_key=zH1fd1S5RPAAGv4Z4vA4qHL45kEicv3C&limit=25&rating=g"
     )
-      .then(response => response.json())
-      .then(responseData => setGifs(responseData)).catch(error => console.log("Error fetching and Parsing data", error))
+      .then((response) => response.json())
+      .then((responseData) => setGifs(responseData))
+      .catch((error) => console.log("Error fetching and Parsing data", error));
   }, []);
 
   return (
