4db433e65a1468e9b4d64431b8c373c9b812ff42
Sprof22 <richmondelaigwu@gmail.com>
2023-02-08 20:34:07 +0100

    remember

--- BEGIN/practice-state/src/components/StarRating.js
+++ BEGIN/practice-state/src/components/StarRating.js
@@ -2,16 +2,34 @@ import React, { useState } from "react";
 import Star from "./Star";
 const StarRating = () => {
   // Initialize a 'courseRating' state
+  const [courseRating, setCourseRating] = useState(0)
   
 
   // Write a function that returns 5 Star components
-  
+  const renderStars = () => {
+    const stars = [];
+    const maxStars = 5;
+    for (let i = 0; i < maxStars; i++){
+      stars.push(
+        <Star 
+          isSelected={courseRating > i}
+          setRating={()=> handleCourseRating(i + 1)}
+          key={i}
+        />
+      )
+    }
+    return stars
+  }
   // Write an event handler that updates the courseRating state.
   // Pass the function to a Star component via props
+  const handleCourseRating = (rating) => {
+    setCourseRating(rating)
+  }
 
   return (
     <ul className="course--stars">
       {/* Render the Star components */}
+      {renderStars()}
       
     </ul>
   );
