d4686079cab86bd027069313549a65c8e72d01ab
Sprof22 <richmondelaigwu@gmail.com>
2023-02-09 12:56:38 +0100

    hey

--- gif-search/src/Components/SearchForm.js
+++ gif-search/src/Components/SearchForm.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
 const SearchForm = props => {
-  const [searchText, setSearchText] = useState('');
+  const [searchTextInput, setSearchTextInput] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
-    props.changeQuery(searchText)
+    props.changeQuery(searchTextInput)
     e.currentTarget.reset()
   }
 
@@ -13,7 +13,7 @@ const SearchForm = props => {
     <form className="search-form" onSubmit={e => handleSubmit(e)} >
       <label className="is-hidden" htmlFor="search">Search</label>
       <input type="search" 
-             onChange={e => setSearchText(e.target.value)}
+             onChange={e => setSearchTextInput(e.target.value)}
              name="search" 
              placeholder="Search..." />
       <button type="submit" id="submit" className="search-button"><i className="material-icons icn-search">search</i></button>
