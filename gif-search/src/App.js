65dcfe7a4b23089416e2a58de3d85e763a3dd1ce
Sprof22 <richmondelaigwu@gmail.com>
2023-02-09 13:41:22 +0100

    push please

--- gif-search/src/App.js
+++ gif-search/src/App.js
@@ -7,20 +7,26 @@ function App() {
   const [gifs, setGifs] = useState([]);
   const [query, setQuery] = useState('');
   useEffect(() => {
+    let activeFetch = true;
     axios
       .get(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=zH1fd1S5RPAAGv4Z4vA4qHL45kEicv3C&limit=25&rating=g`)
       .then(response => {
         // handle success
-        setGifs(response.data.data)
+        if (activeFetch){
+          setGifs(response.data.data)
+        }
       })
       .catch(error =>  {
         // handle error
         console.log("Error fetching and Parsing Data",error);
-      });
+      })
     
+    
+    return () => {activeFetch = false}
   }, [query]);
 const handleQueryChange = (searchText) => {
   setQuery(searchText)
+  
 }
   return (
     <div>
