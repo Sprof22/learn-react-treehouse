d2538e5e9455c84ca1cf04b64d4313f30a694a84
Sprof22 <richmondelaigwu@gmail.com>
2023-02-11 20:21:42 +0100

    because why not

--- course-directory/src/components/Courses.js
+++ course-directory/src/components/Courses.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Navigate, NavLink, Route, Routes } from "react-router-dom";
+import CSS from "./courses/CSS";
+import HTML from "./courses/HTML";
+import JavaScript from "./courses/JavaScript";
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
-      <h2>Courses</h2> 
+      <h2>Courses</h2>
       <ul className="course-nav">
-        <li><a href='#'>HTML</a></li>
-        <li><a href='#'>CSS</a></li>
-        <li><a href='#'>JavaScript</a></li>
+        <li>
+          <NavLink to="html">HTML</NavLink>
+        </li>
+        <li>
+          <NavLink to="css">CSS</NavLink>
+        </li>
+        <li>
+          <NavLink to="javascript">JavaScript</NavLink>
+        </li>
       </ul>
     </div>
+    <Routes>
+      
+    </Routes>
   </div>
 );
 
