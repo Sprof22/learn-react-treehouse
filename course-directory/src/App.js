b08c4d6164b010e7b594b65733b39c0781d0c50d
Sprof22 <richmondelaigwu@gmail.com>
2023-02-11 20:21:44 +0100

    this that good

--- course-directory/src/App.js
+++ course-directory/src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Courses from "./components/Courses";
+import HTML from "./components/courses/HTML";
+import CSS from "./components/courses/CSS";
+import JavaScript from "./components/courses/JavaScript";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Teachers from "./components/Teachers";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="container">
-    <Header />
-  <Routes>
-    <Route path="/" element={<Home />}/>
-    <Route path="about" element={<About />}/>
-    <Route path="teachers" element={<Teachers />}/>
-    <Route path="courses" element={<Courses />}/>
-  </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="teachers" element={<Teachers />} />
+        <Route path="courses/*" element={<Courses />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
