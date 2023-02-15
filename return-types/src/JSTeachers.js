836d2c96546b5a58e3180fef35d7555a96b6e8b0
Sprof22 <richmondelaigwu@gmail.com>
2023-02-15 17:21:51 +0100

    check please

---
+++ return-types/src/JSTeachers.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const JSTeacher = () => {
+  return (
+    <>
+      <li key={1}>Treasure</li>
+      <li key={2}>Guil</li>
+      <li key={3}>James</li>
+    </>
+  );
+};
+
+export default JSTeacher;
