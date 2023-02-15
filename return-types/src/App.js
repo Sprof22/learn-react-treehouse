fac58cc8891ef785b2553de63c1fc6924fb97c16
Sprof22 <richmondelaigwu@gmail.com>
2023-02-15 18:55:13 +0100

    nice

--- return-types/src/App.js
+++ return-types/src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
 
-import Header from './Header';
-import Teachers from './Teachers';
-import Footer from './Footer';
+import Header from "./Header";
+import Teachers from "./Teachers";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        {ReactDOM.createPortal(
+          <h1>Hello from inside the portal</h1>,
+          document.getElementById('my-portal')
+        )}
         <Header />
         <Teachers />
         <Footer />
