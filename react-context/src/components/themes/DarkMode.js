e8be6b7a512f6a1c10d84f4900e2c765c4c5c8b1
Sprof22 <richmondelaigwu@gmail.com>
2023-02-15 02:29:23 +0100

    hey

--- react-context/src/components/themes/DarkMode.js
+++ react-context/src/components/themes/DarkMode.js
@@ -1,12 +1,16 @@
+import { useContext } from "react";
+import ThemeContext from "../../context/ThemeContext";
+
 const DarkMode = (props) => {
+  const {isDarkMode, toggleDarkMode} = useContext(ThemeContext)
   return (
     <div>
       <h3>Dark Mode</h3>
       <input
         type='checkbox'
         className="darkMode-selector"
-        checked={props.isDarkMode}
-        onChange={() => props.toggleDarkMode()} />
+        checked={isDarkMode}
+        onChange={() => toggleDarkMode()} />
     </div>
   );
 }
